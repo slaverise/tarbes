d07330aa029c3048a7d18cc11135eeac38994290
Manjar <slaverise@gmail.com>
2022-05-22 18:24:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import Contract from "./Contract.js";
+
+export default function App() {
+  return <Contract />;
+}
---
+++ src/Contract.js
@@ -0,0 +1,13 @@
+function Contract({ name }) {
+  return (
+    <>
+      <h1>Contrato de Arrendamiento</h1>
+      <h1> de parte de la vivienda</h1>
+      <h1>sita en Huesca, Calle Tarbes 5, ESC. DCHA., 3º C</h1>
+      <h3>En Madrid a 1 de abril de 2022</h3>
+      <h2>Reunidos</h2>
+    </>
+  );
+}
+
+export default Contract;
